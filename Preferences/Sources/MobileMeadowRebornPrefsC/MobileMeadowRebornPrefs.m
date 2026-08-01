f887bc1d9927edab53089864d015cf86be2e1df1
hf805864818 <hf805864818@users.noreply.github.com>
2026-08-01 17:52:58 +0000

    fix: 移除 respring() 头文件定义，修复重复符号错误

--- Preferences/Sources/MobileMeadowRebornPrefsC/MobileMeadowRebornPrefs.m
+++ Preferences/Sources/MobileMeadowRebornPrefsC/MobileMeadowRebornPrefs.m
@@ -1,1 +1,17 @@
-#import "UIFontTTF/UIFont-TTF.m"
+#import "MobileMeadowRebornPrefs.h"
+
+// Thanks to @Nightwind for his respring method (changed from "sbreload" to "killall SpringBoard")
+void respring(void) {
+    extern char **environ;
+    const char *args[] = {"killall", "SpringBoard", NULL};
+    pid_t pid;
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if ([fileManager fileExistsWithPath:@"/var/Liy/.procursus_strapped"] || [fileManager fileExistsWithPath:@"/var/jb/.procursus_strapped"]) {
+        posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)args, environ);
+        return;
+    }
+
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, environ);
+}
--- Preferences/Sources/MobileMeadowRebornPrefsC/include/MobileMeadowRebornPrefs.h
+++ Preferences/Sources/MobileMeadowRebornPrefsC/include/MobileMeadowRebornPrefs.h
@@ -4,18 +4,5 @@
 #import "UIFontTTF/UIFont-TTF.h"
 
 // Thanks to @Nightwind for his respring method (changed from "sbreload" to "killall SpringBoard")
-void respring() {
-    extern char **environ;
-    const char *args[] = {"killall", "SpringBoard", NULL};
-    pid_t pid;
-
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-
-    if ([fileManager fileExistsAtPath:@"/var/Liy/.procursus_strapped"] || [fileManager fileExistsAtPath:@"/var/jb/.procursus_strapped"]) {
-        posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)args, environ);
-        return;
-    }
-
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, environ);
-}
+void respring(void);
 
